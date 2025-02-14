01d99e6e91cb2c996f6c42cc8245732d65d2d956
TonyTwoko <132404498+TonyTwoko@users.noreply.github.com>
2025-02-14 03:26:46 +0300

    V.2

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello Kata!");
+        System.out.print("Hello Kata");
+        System.out.println(" !");
     }
 }
